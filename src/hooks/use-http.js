75703517857d0f1b9350a96d42aed61ff8899936
Oriol <purullurrea@gmail.com>
2023-05-14 21:34:25 +0200

    v0.4.0
    custom hook finished.
    section 15 finished.

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
 
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (taskText) => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+
     setIsLoading(true);
     setError(null);
+
     try {
       const response = await fetch(
         requestConfig.url, {
@@ -29,10 +31,10 @@ const useHttp = (requestConfig, applyData) => {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+    
+  }, []);
 
   return {
-
     isLoading: isLoading,
     error: error,
     sendRequest: sendRequest
